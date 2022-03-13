f87b4f6a112fec4b3a0e4fff4f15d8cdfa33ba51
James <james@jastewart.co.uk>
2022-03-13 20:02:14 +0000

    Pull in from DB

---
+++ server/app.ts
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const { connectToServer, getDb } = require('./db.ts');
+const cors = require("cors");
+app.use(cors());
+
+app.get("/find", async function (req, res) {
+  const projection = {
+    _id: 0,
+    word: 1,
+    example_sentence: 1,
+    translation: 1,
+  };
+
+  console.log("SEARCH STRING", req.query.searchString);
+
+    getDb()
+    .collection("words")
+    .find({ word: {$regex: `^${req.query.searchString}`}})
+    .project(projection)
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!", err);
+     } else {
+        console.log(result);
+        res.json(result);
+      }
+    });
+});
+
+
+const PORT = 8081;
+app.listen(PORT, async function(err){
+
+  if (err) console.log("Error in server setup")
+    await connectToServer(() => console.log("Successfully connected"));
+    console.log("Server listening on Port", PORT);
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+type ShetlandWord = {
+  word: string;
+  translation: string;
+  example_sentence: string
+  type: string
+};
 
 function App() {
+  const [wordList, setWordList] = useState([]);
+  const [foundWords, setFoundWords] = useState<any>([]);
+
+  const fetchWords = async (searchString: string) => {
+    fetch(`http://localhost:8081/find?searchString=${searchString}`)
+      .then(res => res.json())
+      .then(data => setFoundWords(data));
+  }
+
   const debounce = (fn: Function, ms = 300) => {
     let timeoutId: ReturnType<typeof setTimeout>;
     return function (this: any, ...args: any[]) {
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => fn.apply(this, args), ms);
     };
-  };
-
-  type ShetlandWord = {
-    word: string;
-    translation: string;
-    example_sentence: string
-    type: string
   };
 
   const words: ShetlandWord[] = [
@@ -32,10 +41,11 @@ function App() {
   ];
 
   const searchForWord = (searchString: string) => {
-    const foundWords = words.filter(w => `${w.word} ${w.translation} ${w.example_sentence}`.toLowerCase().includes(searchString.toLowerCase()))
+    fetchWords(searchString);
+    // const foundWords = words.filter(w => `${w.word} ${w.translation} ${w.example_sentence}`.toLowerCase().includes(searchString.toLowerCase()))
 
-    if (foundWords.length === 0) console.log("Not found");
-    else console.log(foundWords);
+    // if (foundWords.length === 0) console.log("Not found");
+    // else console.log(foundWords);
   }
 
   const searchWord = debounce((searchString: string) => {
@@ -56,6 +66,12 @@ function App() {
         <div className="absolute top-4 right-3">
           <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
         </div>
+
+        {foundWords && foundWords.map((foundWord: ShetlandWord) => (
+          <span key={foundWord.word}>
+            { foundWord.word }
+          </span>
+        ))}
       </div>
 
     </div>
