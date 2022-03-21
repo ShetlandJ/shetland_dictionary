b926471ca2ab1219f49a08551c056ae84fb4009f
James <james@jastewart.co.uk>
2022-03-21 07:43:45 +0000

    =add

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,10 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
 const app = express();
 const { connectToServer, getDb } = require('./db.ts');
 const cors = require("cors");
 app.use(cors());
 
-app.get("/find", async function (req, res) {
+app.get("/find", async function (req: Request, res: Response) {
   const projection = {
     _id: 0,
     word: 1,
@@ -29,9 +29,9 @@ app.get("/find", async function (req, res) {
     .collection("words")
     .find(searchOr)
     .project(projection)
-    .toArray(function (err, result) {
+    .toArray(function (err: any, result: Array<any>) {
       if (err) {
-        res.status(400).send("Error fetching listings!", err);
+        res.status(400).send("Error fetching listings!");
       } else {
         const payload = result
         .map(word => ({
@@ -46,8 +46,7 @@ app.get("/find", async function (req, res) {
 
 
 const PORT = 8081;
-app.listen(PORT, async function (err) {
-  if (err) console.log("Error in server setup")
+app.listen(PORT, async function () {
   await connectToServer(() => console.log("Successfully connected"));
   console.log("Server listening on Port", PORT);
-})
+});
